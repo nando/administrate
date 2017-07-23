36b36b53e5516a2f0aebfebc0b16a6d0e0f8ddfd
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2017-07-23 11:43:23 +0200

    using 2 scopes, w argument & "wildcarded" features

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -92,6 +92,80 @@ feature "Search" do
     expect(page).not_to have_content(unsubscribed_matched.name)
   end
 
+  scenario "admin searches a word inside two model scopes", :js do
+    searching_for = "Lua"
+    query = "scope:subscribed scope:old #{searching_for}"
+    subscribed_unmathed = create(
+      :customer,
+      name: "Dan Croak",
+      email_subscriber: true)
+    unsubscribed_matched = create(
+      :customer,
+      name: "#{searching_for} Doe",
+      email_subscriber: false)
+    subscribed_match_but_new = create(
+      :customer,
+      created_at: 1.day.ago,
+      name: "#{searching_for} New",
+      email_subscriber: true)
+    subscribed_and_old_match = create(
+      :customer,
+      created_at: 5.years.ago,
+      name: "#{searching_for} Miaus",
+      email_subscriber: true)
+
+    visit admin_customers_path
+    fill_in :search, with: query
+    submit_search
+
+    page.within("table tbody", match: :first) do
+      expect(page).to have_content(subscribed_and_old_match.name)
+    end
+    expect(page).not_to have_content(subscribed_unmathed.name)
+    expect(page).not_to have_content(unsubscribed_matched.name)
+    expect(page).not_to have_content(subscribed_match_but_new.name)
+  end
+
+  scenario "admin searches using a model scope w/ an argument", :js do
+    query = "scope:name_starts_with(L)"
+    match = create(
+      :customer,
+      name: "Lua Miaus")
+    unmatch = create(
+      :customer,
+      name: "John Doe")
+
+    visit admin_customers_path
+    fill_in :search, with: query
+    submit_search
+
+    page.within("table tbody", match: :first) do
+      expect(page).to have_content(match.name)
+    end
+    expect(page).not_to have_content(unmatch.name)
+  end
+
+  scenario "admin searches using a 'wildcarded' scope", :js do
+    query = "name_starts_with:ZZ"
+    match = create(
+      :customer,
+      name: "ZZTop")
+    unmatch = create(
+      :customer,
+      name: "John Doe")
+
+    visit admin_customers_path
+    fill_in :search, with: query
+    submit_search
+    page.within("table tbody", match: :first) do
+      expect(page).to have_content(match.name)
+    end
+    expect(page).not_to have_content(unmatch.name)
+
+    # ...and the wildcarded scope doesn't have its button to be clicked.
+    expect(page).not_to have_content("name_starts_with:*")
+  end
+
   scenario "admin clears search" do
     query = "foo"
     mismatch = create(:customer, name: "someone")
